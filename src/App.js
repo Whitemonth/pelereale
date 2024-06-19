7d3b956779ce0fd8eea0f30e50ec2d7b02b00251
Alex Whitemonth <lxwhlxwh@gmail.com>
2024-06-19 09:56:40 +0300

    stable version without redux

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import { Provider } from "react-redux";
-import { store } from "./redux";
+
 import Goods from "./pages/Goods";
 import About from "./pages/About";
 import Header from "./components/Header/Header";
